d13fc5944895dc8823968f87ded91ef1f64f07e2
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-12-11 17:32:14 +0100

    Broker unit test (#29)

---
+++ tests/connection/broker.cpp
@@ -0,0 +1,47 @@
+
+/* Copyright (c) 2014, Human Brain Project
+ *                     Stefan.Eilemann@epfl.ch
+ */
+
+#include "../broker.h"
+#include <zeq/connection/broker.h>
+
+#include <lunchbox/sleep.h>
+#include <lunchbox/thread.h>
+#include <lunchbox/servus.h>
+#include <boost/bind.hpp>
+
+BOOST_AUTO_TEST_CASE(test_broker)
+{
+    lunchbox::RNG rng;
+    const unsigned short port = (rng.get<uint16_t>() % 60000) + 1024;
+    const std::string& portStr = boost::lexical_cast< std::string >( port );
+    zeq::Subscriber subscriber( lunchbox::URI( "foo://localhost:" + portStr ));
+    BOOST_CHECK( subscriber.registerHandler( zeq::vocabulary::EVENT_CAMERA,
+                                      boost::bind( &test::onCameraEvent, _1 )));
+
+    // Using a different scheme so zeroconf resolution does not work
+    zeq::Publisher publisher( lunchbox::URI( "bar://*:" + portStr ));
+
+    // Using the connection broker in place of zeroconf
+    const std::string& brokerPort = boost::lexical_cast< std::string >( port+1 );
+    zeq::connection::Broker broker( std::string( "*:" ) + brokerPort,
+                                    subscriber );
+    zeq::connection::Service service;
+    BOOST_CHECK( service.subscribe( std::string( "localhost:" ) + brokerPort,
+                                    publisher ));
+
+    bool received = false;
+    for( size_t i = 0; i < 10; ++i )
+    {
+        BOOST_CHECK( publisher.publish(
+                         zeq::vocabulary::serializeCamera( test::camera )));
+
+        if( subscriber.receive( 100 ))
+        {
+            received = true;
+            break;
+        }
+    }
+    BOOST_CHECK( received );
+}
